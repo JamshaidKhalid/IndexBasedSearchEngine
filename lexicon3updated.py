6cf34bf433adc9e5550c0ceeba59ee2106abd4ba
Muhammad Ahmed Fraz <bhattifraz94@gmail.com>
2021-12-14 20:12:17 +0500

    Update lexicon3updated.py
    Without adding the hitlist all the work is done.

--- lexicon3updated.py
+++ lexicon3updated.py
@@ -1,4 +1,5 @@
 import json
+import re
 import nltk
 import unicodedata
 from nltk.stem import PorterStemmer
@@ -6,75 +7,83 @@ from nltk.stem import LancasterStemmer
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
+from nltk.util import pr
+from collections import defaultdict
+import os
+import time
 
+start = time.time()
+count = [0]
 
 #tokenization start from here
-
-ps = PorterStemmer()
-ss=SnowballStemmer("english")
-#using SnowBall stemmer for better control
-list1=[]
-# Removing the stopword 
-stop_words = set(stopwords.words('english'))
-f = open('airwars.json') 
-# returns JSON object as
-# a dictionary
-list1=[]
-data = json.load(f)
-for i in data:
-  for j in i['content']:
-    list1.append(j)
-    
-    list2String = "".join(list1)
-
-print(list2String)  
-list1String = unicodedata.normalize('NFKD', list2String).encode('ASCII', 'ignore')
-words = word_tokenize(list1String.decode())
-#Tokenizing the whole string
-filtered_sentence = [w for w in words if not w.lower() in stop_words]
-
- 
-filtered_sentence = []
- 
-for w in words:
-    if w not in stop_words:
-        filtered_sentence.append(w)
-
-
-print(words)
-print(filtered_sentence)
-stemmed=" "
-#For stemming of the list
-for token in filtered_sentence:
-     stemmed_word = ss.stem(token)
-     stemmed+=(stemmed_word)+ " "
-
-
-punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~0123456789'''
-finalString = ""
-for char in stemmed:
-          if char not in punctuations:
-           finalString= finalString + char
-
-listFirst = list(finalString.split(" "))
-listFirst=  list(set(listFirst))
-print(listFirst)
-
-dict={}
 tokenID=0
-for element in listFirst:
-  dict.__setitem__(element,tokenID)
-  tokenID+=1
+docID=0
+stemmed=" "
+punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~0123456789'''
+directory = 'DataSet'
+ss=SnowballStemmer("english")
+
+wordsList = []
+previousData={}
+seen = set()
+
+previousReverseData=defaultdict(list)
+previousForwardData=defaultdict(list)
+lexiconFile = open("lexicon.json","w")
+forwardIndexFile = open("forwardIndex.json","w")
+reverseIndexFile = open("reverseIndex.json","w")
+docID=0
+tokenID=0
+for root, dirs, files in os.walk(directory):
 
 
-print(dict)
+    for filename in files:
+        currentFile =(os.path.join(root, filename))
+          
+        print((currentFile))
+       
+        stop_words = set(stopwords.words('english'))
+        f = open(currentFile) 
+        data = json.load(f)
+        for i in range(len(data)):
+       
+        
 
-j = json.dumps(dict)
-with open('lex.json','w') as f:
-  f.write(j)
-  f.close()
-#Removing the duplicates if any in the string
+         list1 = data[i]
+         letters = list1['content']
+         wordsString = "".join(letters)
+         wordsList = wordsString.split()
+        #  Optimized to O(n) 
+         docID+=1
+         
+         for w in wordsList:
+            if ((w not in stop_words) and (w not in  punctuations)):
+                stemmed_word = ss.stem(w)
+                string_encode = stemmed_word.encode("ascii", "ignore")
+                string_decode = string_encode.decode()
+                # Removing the uniCode
+                if string_decode not in previousData:
+                  # Checking for the words 
+                  previousData[string_decode]=tokenID
+                  tokenID+=1
+                  # print(type(previousData[string_decode]))
+                
+                if (string_decode in previousData):
+                    # count[previousData[string_decode]] += 1
+                    if previousData[string_decode] not in previousForwardData[docID]:
+                     previousForwardData[docID].append(previousData[string_decode])
+                if (string_decode in previousData):
+                            previousReverseData[previousData[string_decode]].append(docID)
 
-# final = list(dict.fromkeys(stemmed))
-# print(final)
+for i in previousReverseData:   
+ previousReverseData[i]  = list(dict.fromkeys(previousReverseData[i]))                
 
+
+json.dump(previousData,lexiconFile)
+json.dump(previousForwardData,forwardIndexFile)
+json.dump(previousReverseData,reverseIndexFile)
+lexiconFile.close()
+reverseIndexFile.close()
+forwardIndexFile.close()
+end = time.time()
+print((end-start)/60)
