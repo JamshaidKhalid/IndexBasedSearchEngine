6a3af1850fe606f1f79620b52e81204a3730afff
Muhammad Ahmed Fraz <bhattifraz94@gmail.com>
2021-12-29 00:06:02 +0500

    title Based search is added
    if the word is in the title it will be shown first

--- searching.py
+++ searching.py
@@ -11,23 +11,38 @@ def singleWordSearch(searchInput):
    searchQuery= ss.stem(searchInput)
 
    start=time.time()
-   
-   wordID=0
-   dataDict={}
-   hitListDict={}
-   wordID = lexFileData[searchQuery]
-   numberOfDocuments=invertedFileData[str(wordID)]
-   docs = OrderedDict(sorted(numberOfDocuments.items(), key=lambda item: len(item[1]), reverse=True))
-
-   for i in docs:
+   try:
+     wordID1=0
+     wordID1= titlelexFileData[searchQuery]
+     numberOfDocuments1=titleinvertedFileData[str(wordID1)]
+     docs1=OrderedDict(sorted(numberOfDocuments1.items(), key=lambda item: len(item[1]), reverse=True))
+     for i in docs1:
       
       dataArray=(metaData[str(i)])
       print("Title")
       print(dataArray[0])
       print("URL")
       print(dataArray[1])
-   end = time.time()
-   print(end-start)
+   except:
+      wordID=0
+
+      wordID = lexFileData[searchQuery]
+
+
+      numberOfDocuments=invertedFileData[str(wordID)]
+      docs = OrderedDict(sorted(numberOfDocuments.items(), key=lambda item: len(item[1]), reverse=True))
+
+
+
+      for i in docs:
+
+         dataArray=(metaData[str(i)])
+         print("Title")
+         print(dataArray[0])
+         print("URL")
+         print(dataArray[1])
+      end = time.time()
+      print(end-start)
 
  except:
    #  Else show that the words didn't exist in our data
@@ -37,7 +52,7 @@ def scoreGenerator(hitlists):
   # Function for generating score having the args of indexes
 
 		score = 0
-
+    
 		n = len(hitlists)
     
     # n will have the lenght of array like if i entered a word chill life
@@ -80,20 +95,32 @@ def scoreGenerator(hitlists):
 ss = SnowballStemmer("english")
 stop_words = set(stopwords.words("english"))
 
-lexiconFile = open("lex.json", "r")
+lexiconFile = open("lexicon.json", "r")
 # forwardIndexFile = open("forwardIndex.json", "r")
-reverseIndexFile = open("invIndex.json", "r")
-metaDataFile = open("metaInfo.json","r")
+reverseIndexFile = open("reverseIndex.json", "r")
+metaDataFile = open("metaData.json","r")
+titleLexiconFile = open("titleLexicon.json", "r")
+# forwardIndexFile = open("forwardIndex.json", "r")
+titleReverseIndexFile = open("titleReverseIndex.json", "r")
+
 lexFileData= json.load(lexiconFile)
 
 # forwardFileData= json.load(forwardIndexFile)
 
 invertedFileData= json.load(reverseIndexFile)
+titlelexFileData= json.load(titleLexiconFile)
+
+# forwardFileData= json.load(forwardIndexFile)
+
+titleinvertedFileData= json.load(titleReverseIndexFile)
+# print(titlelexFileData)
+# print(titleinvertedFileData)
 
 metaData = json.load(metaDataFile)
 innerDict = {}
 docList=[]
 searchInput= input("Enter the word to search in document\n")
+
 searchInput=searchInput.lower()
 # Converting the word into lower case alphabets
 search= searchInput.split()
@@ -104,7 +131,52 @@ for s in range(len(search)):
 
 
 if(len(search)>  1):
+  try:
       try:
+       
+        # if the word didn't find in lexicon then it would show the exception
+        search_word_ids = [titlelexFileData[word] for word in search if ((word not in stop_words))]
+        # Having the word Id's from lexicon of the word entered by user
+        inverted_index_entries = [titleinvertedFileData[str(word_id)] for word_id in search_word_ids if word_id != -1]
+        # Having the indexes and docs from inverted Index
+        
+        docs_with_score = []
+        # iie is for inverted index entries (IIE)
+        for i, iie in enumerate(inverted_index_entries):
+          # Looping the inverted index enteries
+          for doc in iie:
+            # Traversing the docs in dictionary as it will have the format {docID:[indexes]}
+
+            current_doc_hitlists = [iie[doc]]
+            
+            # having the indexes of current document in the loop in current doc hitlist
+          
+            for remaining_iie in inverted_index_entries[i + 1:]:
+              # for the next document to check the gap between words
+              if doc in remaining_iie:
+                # if the doc is in in the remaining inverted index entries
+                current_doc_hitlists.append(remaining_iie[doc])
+                # so append the hilist with that index of the next doc
+                del remaining_iie[doc]
+                # and delete the doc value so that we are done with that doc
+            docs_with_score.append((doc,scoreGenerator(current_doc_hitlists)))
+          
+            # if the two words are in the same doc it will append the indexes of two docs in one list
+            # Appending score with that doc so that we can make sure the search quality
+        sorted(docs_with_score, key=lambda x: x[1])
+        docs_with_score.sort(key=lambda x: x[1], reverse=True)
+        # Sorting it on the base of the score(descending order)
+        
+        # print(docs_with_score)
+        for i in docs_with_score:
+
+          dataArray=(metaData[str(i[0])])
+          print("Title")
+          print(dataArray[0])
+          print("URL")
+          print(dataArray[1])
+      
+      except:
         # if the word didn't find in lexicon then it would show the exception
         search_word_ids = [lexFileData[word] for word in search if ((word not in stop_words))]
         # Having the word Id's from lexicon of the word entered by user
@@ -146,15 +218,9 @@ if(len(search)>  1):
           print(dataArray[0])
           print("URL")
           print(dataArray[1])
-      except:
-        print("The word didn't exist in our data")
-		    
-
-       
-
-
-         
-         
+  except:
+      print("The word is not in our data set!")
+      
        
    # Multiword Search Query will be if it is greater than length 1
 else:
